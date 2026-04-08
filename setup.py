3ee51cd7581b602d438fe589d9d315c7d36610fe
rajeev-k47 <rajeevkumawat2007@outlook.com>
2026-04-09 01:06:07 +0530

    <Init: */*>?

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'posha_robotic_sub'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rajeev',
+    maintainer_email='rajeevkumawat2007@outlook.com',
+    description='Posha autonomous robotic arm planner',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
