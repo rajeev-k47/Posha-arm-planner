65aebf1b0cdbaa57f107da9dd1beae628fd2580e
rajeev-k47 <rajeevkumawat2007@outlook.com>
2026-04-10 04:30:30 +0530

    <Refac: Adds deps>?

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'posha_robotic_sub'
+
+
+def package_files(directory):
+    files = []
+    for path, _, filenames in os.walk(directory):
+        if not filenames:
+            continue
+        files.append((os.path.join('share', package_name, path), [
+                     os.path.join(path, name) for name in filenames]))
+    return files
+
 
 setup(
     name=package_name,
@@ -10,13 +23,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+    ] + package_files('config') + package_files('resource'),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='rajeev',
     maintainer_email='rajeevkumawat2007@outlook.com',
-    description='Posha autonomous robotic arm planner',
-    license='TODO: License declaration',
+    description='Posha autonomous robotic arm path planning.',
+    license='MIT',
     extras_require={
         'test': [
             'pytest',
@@ -25,6 +39,7 @@ setup(
     entry_points={
         'console_scripts': [
             'generate_artifacts = posha_robotic_sub.generate_artifacts:main',
+            'simulate_assignment = posha_robotic_sub.simulate_arm:main',
         ],
     },
 )
